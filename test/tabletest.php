942784a06f71346972f1bf2d0f525e000572006c
zhuo <lwzhuo@live.com>
2019-05-02 23:07:45 +0800

    初步实现简单table注入

--- test/tabletest.php
+++ test/tabletest.php
@@ -1,1 +1,20 @@
 <?php
+include "../util/TableContainer.php";
+
+$head = array("aa","bb","cc","dd");
+$data = array(
+    array("a1","b1","c1","d1"),
+    array("a2","b2","c2","d2"),
+    array("a3","b3","c3","d3")
+);
+
+$tableObj = new TableContainer($head,$data);
+?>
+
+<html>
+    <body>
+    <?php
+        echo $tableObj->makeTable();
+    ?>
+    </body>
+</html>
--- util/TableContainer.php
+++ util/TableContainer.php
@@ -1,7 +1,53 @@
 <?php
 
-
 class TableContainer
 {
+    private $head_array;//标题数组
+    private $data_array;//数据数组
+    private $html;
 
+    function __construct($head_array,$data_array){
+        $this->head_array = $head_array;
+        $this->data_array = $data_array;
+        $this->html = "";
+    }
+
+    function addhead($headrow){
+        $tr_start="<tr>";
+        $tr_end="</tr>";
+        $th_start="<th>";
+        $th_end="</th>";
+
+        $this->html.=$tr_start;
+        foreach($headrow as $data){
+            $this->html.=$th_start;
+            $this->html.=$data;
+            $this->html.=$th_end;
+        }
+        $this->html.=$tr_end;
+    }
+
+    function addtr($data_array){
+        $tr_start="<tr>";
+        $tr_end="</tr>";
+        $td_start="<td>";
+        $td_end="</td>";
+
+        $this->html.=$tr_start;
+        foreach($data_array as $data){
+            $this->html.=$td_start;
+            $this->html.=$data;
+            $this->html.=$td_end;
+        }
+        $this->html.=$tr_end;
+    }
+
+    function makeTable(){
+        $this->html .= "<table>";
+        $this->addhead($this->head_array);
+        for($i=0;$i<count($this->data_array);$i++)
+            $this->addtr($this->data_array[$i]);
+        $this->html .= "</table>";
+        return $this->html;
+    }
 }
